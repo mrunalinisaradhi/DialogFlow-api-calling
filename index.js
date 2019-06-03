cd63007705561669bcbffc2a4538b00b4d14f37a
mrunalinisaradhi <47136248+mrunalinisaradhi@users.noreply.github.com>
2019-06-03 15:29:44 +0530

    request updated.

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const {Suggestions, SignIn, NewSurface, SimpleResponse} = require('actions-on-go
 const app = dialogflow({debug: true, clientId: 'php-dialogflow-by-sri'});
 const functions = require('firebase-functions');
 const fetch = require('isomorphic-fetch');
+const request = require('request');
 
 restService.use(bodyParser.urlencoded({extended: true}));
 restService.use(bodyParser.json({type: 'application/json'}));
@@ -17,23 +18,31 @@ restService.use(bodyParser.json({type: 'application/json'}));
 // Welcome Intent
 app.intent('Default Welcome Intent', (conv) => {
 	//rest api to get data from strapi => response you add in conv.ask(....)
-	var Request = require("request");
+	/*var Request = require("request");
     Request.get({
-    "headers": { "content-type": "application/json" },
-    "url": "https://cms-dialogflow.herokuapp.com/Responses/14",
-    "body": JSON.stringify({
-       "response":"Hi! How can I help you?"
-    })
-    }, (error, response, body) => {
-    if(error) {
-     return console.dir(error);
-    }
-    console.dir(JSON.parse(body));
-	var data= JSON.parse(body);
-});
+		"headers": { "content-type": "application/json" },
+		"url": "https://cms-dialogflow.herokuapp.com/Responses/14",
+		"body": JSON.stringify({
+		   "response":"Hi! How can I help you?"
+		})
+		}, (error, response, body) => {
+		if(error) {
+		 return console.dir(error);
+		}
+		console.dir(JSON.parse(body));
+		var data= JSON.parse(body);
+	});
 	//conv.ask("Greetings! Message from static webhook");
-	conv.ask('Request.post(data)');
+	conv.ask(Request.post(data));
 	conv.add(new Suggestions('Sign in'));    
+	*/
+	
+	
+	request("https://cms-dialogflow.herokuapp.com/Responses/14", { json: true }, (err, res, body) => {
+	  if (err) { return console.log(err); }
+	  console.log(body);
+	});
+
 });
 
 restService.post('/v2/webhook', app) ;
